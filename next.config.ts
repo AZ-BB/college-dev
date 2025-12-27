f24f318ee7c50fb5fae949834814541cca6c0069
anas12312 <anashesham000@gmail.com>
2025-12-27 18:37:34 +0200

    feat: changed the upload action to api

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  experimental: {
-    // Allow larger body sizes for file uploads (up to 10MB)
-    bodySizeLimit: '10mb',
-  },
+  reactCompiler: true
 };
 
 export default nextConfig;
